99b5926b34d9f3b4c93caaa66b723cb038b16863
CCT3000 <cct3000@gmail.com>
2013-07-06 06:07:59 +0000

    web.js

--- web.js
+++ web.js
@@ -1,9 +1,17 @@
+var fs= require('fs');
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var readfile = fs.readFileSync("index.html");
+
+var buf = new Buffer(256, "utf-8");
+
+var resp = buf.toString("utf-8",0,40);
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(resp);
 });
 
 var port = process.env.PORT || 5000;
