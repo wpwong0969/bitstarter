4aff857a5c1c81075958f23a684558a027e6a78d
CCT3000 <cct3000@gmail.com>
2013-07-07 23:05:29 +0000

    update grader 08/07/2013 1st time

--- grader.js
+++ grader.js
@@ -26,6 +26,7 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://nameless-lowlands-7086.herokuapp.com"
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -65,8 +66,15 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_file>', 'Path to http://nameless-lowlands-7086.herokuapp.com', URL_DEFAULT) 
         .parse(process.argv);
+    if (program.url){
+        var url = program.url.toString();
+        rest.get(url).on('complete', function(result,respone){
+        var checkJson = checkHtmlFile(program.url, program.checks);
+        });
     var checkJson = checkHtmlFile(program.file, program.checks);
+   
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
