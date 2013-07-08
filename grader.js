6abacea9297cabdc31284f867025464f2e34a298
CCT3000 <cct3000@gmail.com>
2013-07-08 02:00:53 +0000

    update grader 08-7-2013 2nd time

--- grader.js
+++ grader.js
@@ -3,14 +3,11 @@
 Automatically grade files for the presence of specified HTML tags/attributes.
 Uses commander.js and cheerio. Teaches command line application development
 and basic DOM parsing.
-
 References:
-
  + cheerio
    - https://github.com/MatthewMueller/cheerio
    - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
    - http://maxogden.com/scraping-with-node.html
-
  + commander.js
    - https://github.com/visionmedia/commander.js
    - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
@@ -20,7 +17,7 @@ References:
    - https://developer.mozilla.org/en-US/docs/JSON
    - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
 */
-
+var rest = require('restler');
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
@@ -72,7 +69,7 @@ if(require.main == module) {
         var url = program.url.toString();
         rest.get(url).on('complete', function(result,respone){
         var checkJson = checkHtmlFile(program.url, program.checks);
-        });
+        })};
     var checkJson = checkHtmlFile(program.file, program.checks);
    
     var outJson = JSON.stringify(checkJson, null, 4);
